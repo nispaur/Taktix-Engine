92063e91199609bdf0015647cedd08a1216447e6
Matthieu LOCUSSOL <dev.matthieu.locussol@gmail.com>
2017-06-22 21:24:24 +0200

    Modified minimal application to automatically close after 3 seconds

--- Tests/Minimal_Application/main.cpp
+++ Tests/Minimal_Application/main.cpp
@@ -9,10 +9,17 @@ int main( int argc, char * argv[] )
     sf::CircleShape shape( 100.0f );
     shape.setFillColor( sf::Color::Green );
 
+    sf::Clock clock;
+
     while ( window.isOpen() )
     {
         sf::Event event;
-        
+
+        if ( clock.getElapsedTime().asSeconds() >= 3.0f )
+        {
+            window.close();
+        }
+
         while ( window.pollEvent( event ) )
         {
             if ( event.type == sf::Event::Closed )
