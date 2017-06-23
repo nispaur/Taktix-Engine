15f3e7a4dc3b728c646dfb85dce19ca8a59d24c0
Matthieu LOCUSSOL <dev.matthieu.locussol@gmail.com>
2017-06-23 11:05:53 +0200

    Updated main to run TaktixEngine instance

--- Application/main.cpp
+++ Application/main.cpp
@@ -1,43 +1,11 @@
-#include <iostream>
-
-#include <SFML/Graphics.hpp>
+#include <Engine/TaktixEngine.hpp>
 
 int main( int argc, char * argv[] )
 {
     ( void ) argc;
     ( void ) argv;
 
-    sf::RenderWindow window( sf::VideoMode(640, 480), "Taktix-Engine" );
-
-    sf::Image logoImage;
-    sf::Texture logoTexture;
-    sf::Sprite logoSprite;
-
-    logoImage.loadFromFile( "Assets/Logos/logo_doxygen.png" );
-    logoTexture.loadFromImage( logoImage );
-    logoSprite.setTexture( logoTexture, true );
-
-    logoSprite.setPosition( ( window.getSize().x - logoSprite.getGlobalBounds().width ) / 2,
-                            ( window.getSize().y - logoSprite.getGlobalBounds().height ) / 2 );
-
-    while (window.isOpen())
-    {
-        sf::Event event;
-
-        while ( window.pollEvent( event ) )
-        {
-            if ( event.type == sf::Event::Closed )
-            {
-                window.close();
-            }
-        }
-
-        window.clear();
-
-        window.draw( logoSprite );
-
-        window.display();
-    }
+    Tx::Engine::TaktixEngine::instance()->run();
 
     return 0;
 }
